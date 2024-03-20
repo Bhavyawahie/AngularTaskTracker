4e134869e39c0b05f05ca62acc735791992a385d
Bhavya Wahie <bhavyawahie@gmail.com>
2024-03-20 19:45:35 +0530

    task services added and separated

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IconDefinition, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Task } from '../../Task';
 
 @Component({
-  selector: 'app-task-item',
-  templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+	selector: 'app-task-item',
+	templateUrl: './task-item.component.html',
+	styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
+	@Input() task: Task;
+	faTimes:IconDefinition = faTimes;
+	constructor() { 
+		this.task = {
+			text: "", 
+			day: "", 
+			reminder: false
+		};
+	}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
